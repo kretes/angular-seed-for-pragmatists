874315969229b2aa59c42fb9bea89e23a4090a2b
Tomasz Bartczak <tomasz.bartczak@pragmatists.pl>
2013-06-21 15:29:21 +0200

    1st version

---
+++ install_all.sh
@@ -0,0 +1,7 @@
+apt-get update
+apt-get install -y python-software-properties python g++ make
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get update
+apt-get install -y nodejs
+npm install karma@0.9.2
+https://github.com/vetik32/angular-seed.git
