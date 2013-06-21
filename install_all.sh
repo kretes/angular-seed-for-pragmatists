704841936064b1afd75fa44f3c41d9e9390ad3e8
Tomasz Bartczak <tomasz.bartczak@pragmatists.pl>
2013-06-21 20:58:23 +0200

    updated installation script to setup everything properly

--- install_all.sh
+++ install_all.sh
@@ -2,6 +2,8 @@ apt-get update
 apt-get install -y python-software-properties python g++ make
 add-apt-repository -y ppa:chris-lea/node.js
 apt-get update
-apt-get install -y nodejs
-npm install karma@0.9.2
-https://github.com/vetik32/angular-seed.git
+apt-get install -y nodejs git libfontconfig1
+npm install -g karma@0.9.2
+npm install -g karma-phantomjs-launcher
+git clone https://github.com/kretes/angular-seed-karma-092-phantomjs.git /vagrant/project
+
